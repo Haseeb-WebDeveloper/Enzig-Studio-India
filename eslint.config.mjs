1f09815d0ff73ae72a56cacde33f630864259c15
Haseeb Ahmed Raza Khan <web.dev.haseeb@gmail.com>
2025-03-23 17:04:34 +0500

    Updated project structure and dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      semi: ["warn", "always"],
+      quotes: ["warn", "double"],
+    },
+  }),
+];
 
 export default eslintConfig;
---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+
+
+
+export const metadata: Metadata = {
+  title: "Enzig India Agency",
+  description: "Enzig India Agency",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={GeistSans.className}>
+      <body className="antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
